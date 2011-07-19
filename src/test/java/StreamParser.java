f631ff3b1f4f11055845444d94b3fbb992ebf779
Ron van Weverwijk <git@weverwijk.net>
2011-07-19 19:38:42 +0200

    batchimporter

--- src/test/java/StreamParser.java
+++ src/test/java/StreamParser.java
@@ -1,18 +1,25 @@
 import com.xebia.xke.benchmark.model.SimpleFlut;
 import com.xebia.xke.benchmark.service.BenchmarkService;
+import org.neo4j.graphdb.index.BatchInserterIndex;
+import org.neo4j.index.impl.lucene.LuceneBatchInserterIndexProvider;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.impl.batchinsert.BatchInserterImpl;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class StreamParser {
     public static void main(String[] args) throws IOException {
-        EmbeddedGraphDatabase database = new EmbeddedGraphDatabase("/tmp/benchmark/neo");
-        BenchmarkService service = new BenchmarkService(database);
+        BatchInserterImpl batchInserter = new BatchInserterImpl("/tmp/benchmark/neo");
+        LuceneBatchInserterIndexProvider luceneBatchInserterIndexProvider = new LuceneBatchInserterIndexProvider(batchInserter);
+        HashMap<String, String> config = new HashMap<String, String>();
+        config.put("type", "exact");
+        BatchInserterIndex nodeIdIndex = luceneBatchInserterIndexProvider.nodeIndex("NodeIdIndex", config);
 
         InputStream stream = StreamParser.class.getResourceAsStream("/data.json");
 
@@ -28,13 +35,26 @@ public class StreamParser {
                 String longStringAttribute = getTokenValue(tokenizer.nextToken());
                 Long id = new Long(getTokenValue(tokenizer.nextToken()));
                 String shortStringAttribute = getTokenValue(tokenizer.nextToken());
-                SimpleFlut simpleFlut = new SimpleFlut(id, shortStringAttribute, longStringAttribute, intNumber, trueOrFalse);
-                service.saveFlut(simpleFlut);
+                long nodeId = batchInserter.createNode(createHashMap(intNumber, trueOrFalse, longStringAttribute, id, shortStringAttribute));
+                HashMap<String, Object> idMap = new HashMap<String, Object>();
+                idMap.put("id", id);
+
+                nodeIdIndex.add(nodeId, idMap);
             }
             if (rows % 1000 == 0) {
                 System.out.println("number of rows: " + rows);
             }
         }
+    }
+
+    private static HashMap<String, Object> createHashMap(Long intNumber, boolean trueOrFalse, String longStringAttribute, Long id, String shortStringAttribute) {
+        HashMap<String, Object> values = new HashMap<String, Object>();
+        values.put("id", id);
+        values.put("longString", longStringAttribute);
+        values.put("shortString", shortStringAttribute);
+        values.put("intNumber", intNumber);
+        values.put("trueOrFalse", trueOrFalse);
+        return values;
     }
 
     private static String getTokenValue(String i) {
